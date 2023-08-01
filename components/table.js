3368e007a22396d5151aa816c3290972358cd575
katamartin <katasm@gmail.com>
2023-08-01 16:52:35 -0700

    Set up sorting

--- components/table.js
+++ components/table.js
@@ -1,19 +1,42 @@
 import { Column, Row } from '@carbonplan/components'
-import { Box, Flex } from 'theme-ui'
+import { Triangle } from '@carbonplan/icons'
+import { useMemo, useState } from 'react'
+import { Box, Flex, IconButton } from 'theme-ui'
 
 import data from '../data/QA.json'
 import Entry from './entry'
 
-const TableHead = ({ children, ...props }) => {
+const TableHead = ({ children, onClick, active, ...props }) => {
   return (
     <Column as="th" {...props} sx={{ textAlign: 'left', fontSize: 0 }}>
       <Flex
         sx={{
           height: '100%',
           flexDirection: 'column',
-          justifyContent: 'flex-end',
+          alignItems: 'flex-start',
+          justifyContent: 'space-between',
         }}
       >
+        <IconButton
+          sx={{
+            p: 0,
+            width: 10,
+            '&:hover svg': { stroke: 'primary' },
+            cursor: 'pointer',
+          }}
+          onClick={onClick}
+        >
+          <Triangle
+            sx={{
+              transition: 'stroke 0.15s',
+              stroke: active ? 'primary' : 'muted',
+              fill: 'none',
+              width: 10,
+              height: 10,
+            }}
+          />
+        </IconButton>
+
         {children}
       </Flex>
     </Column>
@@ -21,40 +44,81 @@ const TableHead = ({ children, ...props }) => {
 }
 
 const Table = () => {
+  const [sort, setSort] = useState('target')
+
+  const sortedData = useMemo(() => {
+    const sorter = ['target', 'tool'].includes(sort)
+      ? (a, b) => a[sort].localeCompare(b[sort])
+      : (a, b) => b.coverage[sort] - a.coverage[sort]
+    return data.map((d) => d.quant_approach).sort(sorter)
+  }, [sort])
+
   return (
     <Box as="table">
       <Box as="thead">
         <Row as="tr" columns={[6, 8, 10, 10]} sx={{ mb: 4 }}>
-          <TableHead start={1} width={[2]}>
+          <TableHead
+            onClick={() => setSort('target')}
+            active={sort === 'target'}
+            start={1}
+            width={[2]}
+          >
             Target
           </TableHead>
-          <TableHead start={[3]} width={[3]}>
+          <TableHead
+            onClick={() => setSort('tool')}
+            active={sort === 'tool'}
+            start={[3]}
+            width={[3]}
+          >
             Tool
           </TableHead>
-          <TableHead start={[6]} width={[1]}>
+          <TableHead
+            onClick={() => setSort('rock')}
+            active={sort === 'rock'}
+            start={[6]}
+            width={[1]}
+          >
             Rock application
           </TableHead>
-          <TableHead start={[7]} width={[1]}>
+          <TableHead
+            onClick={() => setSort('init_weathering')}
+            active={sort === 'init_weathering'}
+            start={[7]}
+            width={[1]}
+          >
             Initial weathering
           </TableHead>
-          <TableHead start={[8]} width={[1]}>
+          <TableHead
+            onClick={() => setSort('field')}
+            active={sort === 'field'}
+            start={[8]}
+            width={[1]}
+          >
             Field processes
           </TableHead>
-          <TableHead start={[9]} width={[1]}>
+          <TableHead
+            onClick={() => setSort('watershed')}
+            active={sort === 'watershed'}
+            start={[9]}
+            width={[1]}
+          >
             Watershed transport
           </TableHead>
-          <TableHead start={[10]} width={[1]}>
+          <TableHead
+            onClick={() => setSort('ocean')}
+            active={sort === 'ocean'}
+            start={[10]}
+            width={[1]}
+          >
             Ocean storage
           </TableHead>
         </Row>
       </Box>
 
       <Box as="tbody">
-        {data.map((d) => (
-          <Entry
-            key={`${d.quant_approach.target}-${d.quant_approach.tool}`}
-            {...d.quant_approach}
-          />
+        {sortedData.map((d) => (
+          <Entry key={`${d.target}-${d.tool}`} {...d} />
         ))}
       </Box>
     </Box>
