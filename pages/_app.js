6ed4ca5b296ac2fbb72bac9df9fe82e44b8c7b20
katamartin <katasm@gmail.com>
2023-08-03 17:03:41 -0700

    Update description

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
 import theme from '@carbonplan/theme'
 import { Tool } from '@carbonplan/layouts'
+import { Link } from '@carbonplan/components'
 
 const meta = {
   title: 'Quantifying enhanced weathering',
@@ -13,12 +14,22 @@ const meta = {
   summary: 'TK',
 }
 
+const description = (
+  <>
+    This resource allows users to explore what kinds of quantification tools
+    might play a role in estimating carbon removal from enhanced weathering on
+    agricultural soils. It is designed to help a wider community interpret and
+    evaluate proposed approaches to enhanced weathering MRV. Read more in the
+    accompanying <Link href="TK">explainer</Link>.
+  </>
+)
+
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
       <Tool
         meta={meta}
-        description="TK: tool description"
+        description={description}
         contentWidth={[6, 8, 10, 10]}
         descriptionWidth={[6, 7, 7, 7]}
         quickLookStart={9}
